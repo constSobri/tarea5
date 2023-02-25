4e93373062dd215cbfb65a0d0f3bce7bd3f29091
ang <57077187+constSobri@users.noreply.github.com>
2023-02-25 09:44:58 -0600

    primero

---
+++ nombres.js
@@ -0,0 +1,12 @@
+let altura = 176;
+let metros = 1.76;
+let peso = 65.4;
+let alturaRedondeada = metros.toPrecision(2);
+let pesoPorDebajo = peso.toFixed(0);
+
+let max = Number.MAX_VALUE; 
+let suma = max + 1;
+
+console.log(alturaRedondeada);
+console.log(pesoPorDebajo);
+console.log(suma);
